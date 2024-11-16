9340c4b1cd619b5bbe9c7059285313b760fc6f6e
Rafael Bormann Chuede <rafaelchuede@gmail.com>
2024-11-15 21:52:22 -0300

    Janela Pdv inicio

--- src/main/java/com/example/demo1/HelloController.java
+++ src/main/java/com/example/demo1/HelloController.java
@@ -2,8 +2,7 @@ package com.example.demo1;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
+
 
 import java.io.IOException;
 
